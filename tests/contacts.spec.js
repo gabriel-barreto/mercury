beac76d69b47f84f50ea6a836817e0e1c9413bd5
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-06-05 16:28:40 -0300

    Improve fetch tests, add check tests and init test to create

--- tests/contacts.spec.js
+++ tests/contacts.spec.js
@@ -1,4 +1,5 @@
 import chai, { expect } from "chai";
+import moxios from "moxios";
 import sinonChai from "sinon-chai";
 import sinon from "sinon";
 
@@ -72,9 +73,90 @@ describe("Contacts Service", () => {
             // ==> Expect
             expect(httpStub).to.have.been.calledWith(url, options);
         });
+
+        it("shouldn't call get when no receives an email", () => {
+            const email = "";
+            $contacts.fetch(email);
+            expect(httpStub).to.not.have.been.called;
+        });
     });
 
-    describe("Check Method", () => {});
+    describe("Check Method", () => {
+        let contactStub;
 
-    // describe("Check method", () => {});
+        beforeEach(() => {
+            moxios.install($http);
+        });
+
+        afterEach(() => {
+            moxios.uninstall($http);
+        });
+
+        it("should return false when no receives id", async () => {
+            moxios.stubRequest(/recipients/, {
+                status: 200,
+                response: {},
+            });
+
+            const received = await $contacts.check("email@something.com");
+            const expected = false;
+
+            expect(received).to.be.equal(expected);
+        });
+
+        it("should return true when received a contact id", async () => {
+            moxios.wait(() => {
+                const response = {
+                    recipient_count: 1,
+                    recipients: [
+                        {
+                            id: "ZGV2QGVudHJlY2xpcXVlcy5jb20uYnI=",
+                            email: "dev@entrecliques.com.br",
+                            first_name: "Entrecliques",
+                            last_name: "Developer",
+                        },
+                    ],
+                };
+                const request = moxios.requests.mostRecent();
+
+                request.respondWith({ stauts: 200, response });
+            });
+
+            const received = await $contacts.check("dev@entrecliques.com,br");
+            const expected = true;
+
+            expect(received).to.be.equal(expected);
+        });
+    });
+
+    describe("Create method", () => {
+        let httpStub;
+        const contactData = {
+            first_name: "Gabriel",
+            last_name: "Barreto",
+            email: "barreto-gabriel@outlook.com",
+        };
+
+        beforeEach(() => {
+            httpStub = sinon.stub($http, "post");
+            httpStub.resolves({});
+        });
+
+        afterEach(() => {
+            httpStub.restore();
+        });
+
+        it("should call post method from HTTP", () => {
+            $contacts.create({});
+            expect(httpStub).to.have.been.calledOnce;
+        });
+
+        it("should call post method with correct params", () => {
+            const url = "/contactdb/recipients";
+            const data = contactData;
+
+            $contacts.create(data);
+            expect(httpStub).to.have.been.calledWith(url, data);
+        });
+    });
 });
