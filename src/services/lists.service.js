6648af710f4cdd1d4d64b7c41bd47bff4eaa7fe6
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-06-19 11:19:22 -0300

    Add payload validator to service method flow

--- src/services/lists.service.js
+++ src/services/lists.service.js
@@ -1,14 +1,19 @@
 import $http from "./http.service";
+import validator from "../utils/validator";
 
 const basePath = "/contactdb/lists";
+const _required = ["title"];
 
 const create = listData =>
-    $http
-        .post(basePath, listData)
-        .then(({ data = {} }) => data.id || null)
-        .catch(err => {
-            throw err;
-        });
+    new Promise((resolve, reject) => {
+        if (validator.validate(_required, listData)) {
+            return $http
+                .post(basePath, listData)
+                .then(({ data = {} }) => resolve(data.id || null))
+                .catch(reject);
+        }
+        resolve(null);
+    });
 
 const fill = () => {};
 
