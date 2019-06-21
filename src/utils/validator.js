2be489dcaf16c1a8132c008d386627bfb6d4f482
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-06-21 11:28:58 -0300

    Improve validator behavior

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,4 +1,11 @@
 const validate = (props, payload) => {
+    if (!(props instanceof Array))
+        throw new Error("Required props must be an array of fields");
+    else if (props.length < 1) return true;
+    else if (!payload) return false;
+    else if (payload.constructor !== Object) return false;
+    else if (Object.keys(payload).length < 1) return false;
+
     const found = props.filter(each => payload[each]);
     return found.length === props.length;
 };
