04a90a0016162c8ee3a2c7770538df258442f059
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-06-24 12:48:55 -0300

    Create campaign method

--- src/services/campaign.service.js
+++ src/services/campaign.service.js
@@ -1,8 +1,25 @@
 import $http from "./http.service";
+import validator from "../utils/validator";
 
 const basePath = "/campaign";
+const _required = [
+    "title",
+    "subject",
+    "list_ids",
+    "sender_id",
+    "categories",
+    "html_content",
+];
 
-const create = () => $http.post(basePath);
+const create = payload =>
+    new Promise((resolve, reject) => {
+        if (!payload) return reject(false);
+        else if (!validator.validate(_required, payload)) return reject(false);
+        return $http
+            .post(basePath, payload)
+            .then(resolve)
+            .catch(reject);
+    });
 const shoot = () => {};
 
 export default { create, shoot };
