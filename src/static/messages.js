7f7ad2301bdba8196df0c638344903f38b381149
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-07-02 18:53:14 -0300

    Static error messages

---
+++ src/static/messages.js
@@ -0,0 +1,7 @@
+export default {
+    lists_creation:
+        "Ocorreu um erro ao tentar registar a lista, por favor verifique as informações ou tente novamente mais tarde.",
+    template:
+        "Ocorreu um erro ao tentar obter o conteúdo do template informado, por favor, verifique as informações e tente novamente.",
+    settings: "Arquivo de configuração inválido ou não encontrado.",
+};
