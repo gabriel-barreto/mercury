077416423030810d657b5cc296ef70646b5a9bfd
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-07-02 17:47:16 -0300

    Finish settings service

--- src/services/settings.service.js
+++ src/services/settings.service.js
@@ -1,6 +1,36 @@
 import fs from "fs";
-import path from "path";
 
-const parse = () => {};
+const check = path => {
+    try {
+        const stat = fs.lstatSync(path);
+        return stat.isFile();
+    } catch (_) {
+        return false;
+    }
+};
+
+const validate = payload => {
+    const _required = ["title", "subject", "targets", "template", "subject"];
+    const keys = Object.keys(payload);
+    const found = _required.map(each => keys.includes(each));
+
+    return !found.includes(false);
+};
+
+const parse = path => {
+    if (!check(path)) return null;
+
+    const settings = require(path);
+
+    if (!validate(settings)) return null;
+
+    return {
+        tags: settings.tags || [],
+        title: settings.title,
+        subject: settings.subject,
+        targets: settings.targets,
+        template: settings.template,
+    };
+};
 
 export default { parse };
